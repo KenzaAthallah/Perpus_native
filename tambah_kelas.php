efeb44a486dd844ab57979d4bd0cb4cc9388eaac
KenzaAthallah <90818562+KenzaAthallah@users.noreply.github.com>
2021-11-30 18:53:12 +0700

    Create tambah_kelas.php

---
+++ tambah_kelas.php
@@ -0,0 +1,40 @@
+# Perpus_native
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Kelas</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        include "navbar.php";
+    ?>
+    <br></br>
+    <div class="container">
+        <div class="card">
+            <h1 class="card-header">Tambah Kelas</h1>
+            <div class="card-body">
+                <form method="POST" action="proses_tambah_kelas.php">
+                    <div class="mb-3">
+                        <label for="nama_kelas" class="form-label">Nama Kelas</label>
+                        <input type="text" class="form-control" name="nama_kelas" value="" placeholder="Masukkan Nama Kelas" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="kelompok" class="form-label">Kelompok</label>
+                        <input type="text" class="form-control" name="kelompok" placeholder="Masukkan Kelompok" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
