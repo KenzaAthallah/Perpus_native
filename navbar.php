9ab18efd482a85e04a2d609c5db2e533442aa310
KenzaAthallah <90818562+KenzaAthallah@users.noreply.github.com>
2021-11-30 18:46:06 +0700

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,23 @@
+# Perpus_native
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">PERPUSTAKAAN</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="tampil_kelas.php">Data Kelas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tampil_siswa.php">Data Siswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="tampil_buku.php">Data Buku</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
