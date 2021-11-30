9857e2dfa5cd63ee9389c67296b07ad2193a011c
KenzaAthallah <90818562+KenzaAthallah@users.noreply.github.com>
2021-11-30 18:52:33 +0700

    Create tambah_buku.php

---
+++ tambah_buku.php
@@ -0,0 +1,48 @@
+# Perpus_native
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Buku</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+        include "navbar.php";
+    ?>
+    <br></br>
+    <div class="container">
+        <div class="card">
+            <h1 class="card-header">Tambah Buku</h1>
+            <div class="card-body">
+                <form method="POST" action="proses_tambah_buku.php" enctype="multipart/form-data">
+                    <div class="mb-3">
+                        <label for="nama_buku" class="form-label">Nama Buku</label>
+                        <input type="text" class="form-control" name="nama_buku" placeholder="Masukkan Nama Buku" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="pengarang" class="form-label">Pengarang</label>
+                        <input type="text" class="form-control" name="pengarang" placeholder="Masukkan Nama Pengarang" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="deskripsi" class="form-label">Deskripsi</label>
+                        <textarea class="form-control" name="deskripsi" row="3" placeholder="Masukkan Deskripsi Buku" required></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="foto" class="form-label">Foto</label>
+                        <input type="file" class="form-control" name="foto" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
